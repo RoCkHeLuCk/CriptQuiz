220e38f625eee422c8b70ba8395685bae21ae131
RoCkHeLuCk <rockheluck@gmail.com>
2024-08-06 10:42:03 -0300

    update esp32

--- _documents/esp32doit-devkit-v1/src/main.cpp
+++ _documents/esp32doit-devkit-v1/src/main.cpp
@@ -5,12 +5,9 @@ void setup()
    Serial.begin(9600);
    pinMode(LED_BUILTIN, OUTPUT);
    digitalWrite(LED_BUILTIN, LOW);
-   pinMode(A0, ANALOG);
 }
 
 char command = '\0';
-uint16_t temp = 0;
-
 void loop()
 {
 
@@ -22,8 +19,5 @@ void loop()
          case 'd': digitalWrite(LED_BUILTIN, LOW); break;
          case 'l': digitalWrite(LED_BUILTIN, HIGH); break;
       }
-
-      temp = analogRead(A0);
-      Serial.printf("%05.2f\n",temp);
    }
 }
